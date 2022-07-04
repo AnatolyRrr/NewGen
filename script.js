ef805553b43fe6b3da0038c2fdad459612726151
Anatoly Ryazanov <Ryazanov.AO@yandex.ru>
2022-07-04 23:16:53 +0500

    Finally change

--- script.js
+++ script.js
@@ -10,20 +10,18 @@
         { name: "Courses in France", prices: [null, null] },
     ];
 
-    // Варианты цен (фильтры), которые ищет пользователь
-    let requiredRange1 = [null, 200];
-    let requiredRange2 = [100, 350];
-    let requiredRange3 = [200, null];
-
     let courseList = document.querySelector('.courses');
-    let filterButton = document.querySelector('.btn');
+    let findButton = document.querySelector('.btn');
     let priceFrom = document.querySelector('.price-from');
     let priceTo = document.querySelector('.price-to');
 
-
+    priceFrom.value = '';
+    priceTo.value = '';
 
     const filterList = () => {
-        let filteredList = courses.filter((el) => {
+        courseList.innerHTML = '';
+        // Фильтр по input.value
+        const filteredList = courses.filter((el) => {
             if(priceTo.value > null) {
                 return el.prices[0] >= priceFrom.value && el.prices[0] <= priceTo.value && el.prices[1] <= priceTo.value
             } else {
@@ -31,20 +29,22 @@
             }
         })
         
-        filteredList.forEach((element) => {
+        filteredList.forEach((element) => {      
             let course = document.createElement('li')
-            
-            if((element.prices[0] = 0) && (element.prices[1] = 0)) {
+
+            // Преобразование текста без 'null'
+            if(!element.prices[0] && !element.prices[1]) {
                 course.innerText = 'Название:' + ' ' + element.name  + ' ' + '|'  + ' ' + 'Стоимость:' + ' ' + 'Уточняйте';
-            } else if((element.prices[0] > 0) && (element.prices[1] > 0)) {
-                course.innerText = 'Название:' + ' ' + element.name  + ' ' + '|'  + ' ' + 'Стоимость:' + ' ' + 'от' + element.prices[0] + ' ' + 'до' + ' ' + element.prices[1];
-            } else if((element.prices[0] = 0) && (element.prices[1] > 0)) {
+            } else if(element.prices[0] && element.prices[1]) {
+                course.innerText = 'Название:' + ' ' + element.name  + ' ' + '|'  + ' ' + 'Стоимость:' + ' ' + 'от' + ' ' + element.prices[0] + ' ' + 'до' + ' ' + element.prices[1];
+            } else if(!element.prices[0] && element.prices[1]) {
                 course.innerText = 'Название:' + ' ' + element.name  + ' ' + '|'  + ' ' + 'Стоимость:' + ' ' + 'до' + ' ' + element.prices[1];
-            } else if((element.prices[0] > 0) && (element.prices[1] = 0)) {
-                course.innerText = 'Название:' + ' ' + element.name  + ' ' + '|'  + ' ' + 'Стоимость:' + ' ' + 'от' + element.prices[0];
+            } else if(element.prices[0] && !element.prices[1]) {
+                course.innerText = 'Название:' + ' ' + element.name  + ' ' + '|'  + ' ' + 'Стоимость:' + ' ' + 'от' + ' ' + element.prices[0];
             }
+            
             courseList.append(course)
         })
     };
-
-    filterButton.onclick = filterList;
+    
+    findButton.onclick = filterList;
